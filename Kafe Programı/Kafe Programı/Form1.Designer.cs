20f2c001e5337537900b404ff95ce29cbed38a76
hilaldayann <63721685+hilaldayann@users.noreply.github.com>
2021-04-18 17:50:38 +0300

    Kafe Programi

---
+++ Kafe Programı/Kafe Programı/Form1.Designer.cs
@@ -0,0 +1,387 @@
+﻿
+namespace Kafe_Programı
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.checkBox9 = new System.Windows.Forms.CheckBox();
+            this.checkBox10 = new System.Windows.Forms.CheckBox();
+            this.checkBox11 = new System.Windows.Forms.CheckBox();
+            this.checkBox12 = new System.Windows.Forms.CheckBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(54, 21);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Çay";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(12, 39);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(70, 21);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Kahve";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(12, 66);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(58, 21);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "Tost";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(12, 93);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(47, 21);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "Su";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(12, 120);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(80, 21);
+            this.checkBox5.TabIndex = 4;
+            this.checkBox5.Text = "Çikolata";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.CheckedChanged += new System.EventHandler(this.checkBox5_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(122, 11);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(122, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(122, 65);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(122, 92);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(122, 119);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(269, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 17);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "0.75 lira";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(269, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "1 lira";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(269, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "3 lira";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(269, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 17);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "1 lira";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(269, 124);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 17);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "3 lira";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(138, 164);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 193);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(386, 228);
+            this.listBox1.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(21, 424);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(368, 17);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "20 TL ve üzeri %10, 50 TL ve üzeri %20 indirim olacaktır.";
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(442, 38);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(89, 21);
+            this.checkBox6.TabIndex = 18;
+            this.checkBox6.Text = "Pazartesi";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(442, 65);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(53, 21);
+            this.checkBox7.TabIndex = 19;
+            this.checkBox7.Text = "Salı";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(442, 92);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(94, 21);
+            this.checkBox8.TabIndex = 20;
+            this.checkBox8.Text = "Çarşamba";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // checkBox9
+            // 
+            this.checkBox9.AutoSize = true;
+            this.checkBox9.Location = new System.Drawing.Point(442, 119);
+            this.checkBox9.Name = "checkBox9";
+            this.checkBox9.Size = new System.Drawing.Size(94, 21);
+            this.checkBox9.TabIndex = 21;
+            this.checkBox9.Text = "Perşembe";
+            this.checkBox9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.checkBox9.UseVisualStyleBackColor = true;
+            // 
+            // checkBox10
+            // 
+            this.checkBox10.AutoSize = true;
+            this.checkBox10.Location = new System.Drawing.Point(442, 146);
+            this.checkBox10.Name = "checkBox10";
+            this.checkBox10.Size = new System.Drawing.Size(66, 21);
+            this.checkBox10.TabIndex = 22;
+            this.checkBox10.Text = "Cuma";
+            this.checkBox10.UseVisualStyleBackColor = true;
+            // 
+            // checkBox11
+            // 
+            this.checkBox11.AutoSize = true;
+            this.checkBox11.Location = new System.Drawing.Point(442, 173);
+            this.checkBox11.Name = "checkBox11";
+            this.checkBox11.Size = new System.Drawing.Size(93, 21);
+            this.checkBox11.TabIndex = 23;
+            this.checkBox11.Text = "Cumartesi";
+            this.checkBox11.UseVisualStyleBackColor = true;
+            // 
+            // checkBox12
+            // 
+            this.checkBox12.AutoSize = true;
+            this.checkBox12.Location = new System.Drawing.Point(442, 200);
+            this.checkBox12.Name = "checkBox12";
+            this.checkBox12.Size = new System.Drawing.Size(67, 21);
+            this.checkBox12.TabIndex = 24;
+            this.checkBox12.Text = "Pazar";
+            this.checkBox12.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(439, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(174, 17);
+            this.label7.TabIndex = 25;
+            this.label7.Text = "SÜPER İNDİRİM GÜNLERİ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(423, 244);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(193, 194);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 26;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.checkBox12);
+            this.Controls.Add(this.checkBox11);
+            this.Controls.Add(this.checkBox10);
+            this.Controls.Add(this.checkBox9);
+            this.Controls.Add(this.checkBox8);
+            this.Controls.Add(this.checkBox7);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "İNDİRİM KAFE";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.CheckBox checkBox9;
+        private System.Windows.Forms.CheckBox checkBox10;
+        private System.Windows.Forms.CheckBox checkBox11;
+        private System.Windows.Forms.CheckBox checkBox12;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Kafe Programı/Kafe Programı/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kafe_Programı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double tutar = 0;
+        double stutar = 0;
+        double cay, kahve, tost, su, cikolata;
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                textBox1.Enabled = true;
+            }
+            else
+            {
+                textBox1.Enabled = false;
+            }
+        }
+        
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                textBox2.Enabled = true;
+            }
+            else
+            {
+                textBox2.Enabled = false;
+            }
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked)
+            {
+                textBox3.Enabled = true;
+            }
+            else
+            {
+                textBox3.Enabled = false;
+            }
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox4.Checked)
+            {
+                textBox4.Enabled = true;
+            }
+            else
+            {
+                textBox4.Enabled = false;
+            }
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox5.Checked)
+            {
+                textBox5.Enabled = true;
+            }
+            else
+            {
+                textBox5.Enabled = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            if (checkBox1.Checked)
+            {
+                if (textBox1.Text == "")
+                {
+                    MessageBox.Show("Çay Adetini Girmediniz.");
+                }
+                else
+                {
+                    cay = Convert.ToDouble(textBox1.Text);
+                    tutar += cay * 0.75;
+                    listBox1.Items.Add(cay + " Adet Çay " + cay * 0.75 + " TL");
+                }
+            }
+
+            if (checkBox2.Checked)
+            {
+                if (textBox2.Text == "")
+                {
+                    MessageBox.Show("Kahve Adetini Girmediniz.");
+                }
+                else
+                {
+                    kahve = Convert.ToDouble(textBox2.Text);
+                    tutar += kahve * 1;
+                    listBox1.Items.Add(kahve + " Adet Kahve " + kahve * 1 + " TL");
+                }
+            }
+
+            if (checkBox3.Checked)
+            {
+                if (textBox3.Text == "")
+                {
+                    MessageBox.Show("Tost Adetini Girmediniz.");
+                }
+                else
+                {
+                    tost = Convert.ToDouble(textBox3.Text);
+                    tutar += tost * 3;
+                    listBox1.Items.Add(tost + " Adet Tost " + tost * 3 + " TL");
+                }
+            }
+
+            if (checkBox4.Checked)
+            {
+                if (textBox4.Text == "")
+                {
+                    MessageBox.Show("Su Adetini Girmediniz.");
+                }
+                else
+                {
+                    su = Convert.ToDouble(textBox4.Text);
+                    tutar += su * 1;
+                    listBox1.Items.Add(su + " Adet Su " + su * 1 + " TL");
+                }
+            }
+
+            if (checkBox5.Checked)
+            {
+                if (textBox5.Text == "")
+                {
+                    MessageBox.Show("Çikolata Adetini Girmediniz.");
+                }
+                else
+                {
+                    cikolata = Convert.ToDouble(textBox5.Text);
+                    tutar += cikolata * 3;
+                    listBox1.Items.Add(cikolata + " Adet Çikolata " + cikolata * 3 + " TL");
+                }
+            }
+
+            tutar = cay * 0.75 + kahve * 1 + tost * 3 + su * 1 + cikolata * 3;
+
+
+            if (checkBox6.Checked)
+            {
+                listBox1.Items.Add("----------------------------");
+                listBox1.Items.Add("Genel Tutar : " + tutar);
+                stutar = tutar * 0.5;
+                listBox1.Items.Add("Süper Pazartesi İndirimi : " + stutar);
+            }
+
+            if (checkBox7.Checked)
+            {
+                listBox1.Items.Add("----------------------------");
+                listBox1.Items.Add("Genel Tutar : " + tutar);
+                stutar = tutar * 0.1;
+                listBox1.Items.Add("Süper Salı İndirimi : " + stutar);
+            }
+
+            if (checkBox8.Checked)
+            {
+                listBox1.Items.Add("----------------------------");
+                listBox1.Items.Add("Genel Tutar : " + tutar);
+                stutar = tutar * 0.8;
+                listBox1.Items.Add("Süper Çarşamba İndirimi : " + stutar);
+            }
+
+            if (checkBox9.Checked)
+            {
+                listBox1.Items.Add("----------------------------");
+                listBox1.Items.Add("Genel Tutar : " + tutar);
+                stutar = tutar * 0.3;
+                listBox1.Items.Add("Süper Perşembe İndirimi : " + stutar);
+            }
+
+            if (checkBox10.Checked)
+            {
+                listBox1.Items.Add("----------------------------");
+                listBox1.Items.Add("Genel Tutar : " + tutar);
+                stutar = tutar * 0.2;
+                listBox1.Items.Add("Süper Cuma İndirimi : " + stutar);
+            }
+
+            if (checkBox11.Checked)
+            {
+                listBox1.Items.Add("----------------------------");
+                listBox1.Items.Add("Genel Tutar : " + tutar);
+                stutar = tutar * 0.4;
+                listBox1.Items.Add("Süper Cumartesi İndirimi : " + stutar);
+            }
+
+            if (checkBox12.Checked)
+            {
+                listBox1.Items.Add("----------------------------");
+                listBox1.Items.Add("Genel Tutar : " + tutar);
+                stutar = tutar * 0.3;
+                listBox1.Items.Add("Süper Pazar İndirimi : " + stutar);
+            }
+
+            if (tutar >= 20 && tutar < 50)
+            {
+                tutar = tutar * 0.9;
+                listBox1.Items.Add("----------------------------");
+                listBox1.Items.Add("Tutarda %10 indirim yapıldı.");
+                listBox1.Items.Add("İndirimli Tutar : " + tutar);
+                listBox1.Items.Add("----------------------------");
+                listBox1.Items.Add("Genel Tutar : " + (tutar-stutar));
+            }
+
+            else if(tutar >= 50)
+            {
+                tutar = tutar * 0.8;
+                listBox1.Items.Add("----------------------------");
+                listBox1.Items.Add("Tutarda %20 indirim yapıldı.");
+                listBox1.Items.Add("İndirimli Tutar : " + tutar);
+                listBox1.Items.Add("----------------------------");
+                listBox1.Items.Add("Genel Tutar : " + (tutar - stutar));
+            }
+            else
+            {
+                listBox1.Items.Add("----------------------------");
+                listBox1.Items.Add("Genel Tutar : " + tutar);
+                listBox1.Items.Add("----------------------------");
+                listBox1.Items.Add("Genel Tutar : " + (tutar - stutar));
+            }
+
+        }
+    }
+}
